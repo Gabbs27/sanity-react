bbac1f78d0f834c2d1898bd7ae82f7d30a8c4c76
Gabriel <djabreu_@hotmail.com>
2023-01-30 10:52:59 -0400

    Cambios en para diseño homogeneo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,22 +5,26 @@ import { data } from "../assets/data"; // double check the path here
 const Portfolio = () => {
   if (!data) return <div>Loading...</div>; // this will show loading message until data is loaded
   return (
-    <div>
-      <h1 className='text-center text-3xl font-medium'>My Portfolio</h1>
-
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        {data.map((item) => {
-          return (
-            <Card
-              key={item.id}
-              image={item.image}
-              title={item.title}
-              description={item.description}
-              url={item.url}
-              languages={item.languages}
-            />
-          );
-        })}
+    <div className='min-h-screen p-12'>
+      <div className='container mx-auto'>
+        <h2 className='text-3xl flex justify-center'>Portfolio</h2>
+        <h3 className='text-lg text-gray-600 flex justify-center mb-12'>
+          My projects!
+        </h3>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          {data.map((item) => {
+            return (
+              <Card
+                key={item.id}
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                url={item.url}
+                languages={item.languages}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
