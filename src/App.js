14435dc92014350382f9db7a2da1bb54d0214df4
Gabriel <djabreu_@hotmail.com>
2023-03-06 19:48:17 -0400

    Se recuperan los paths.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
+import About from "./components/About.js";
+import Portfolio from "./components/Portfolio.js";
+import Me from "./components/Me.js";
+import Repos from "./components/Repos";
 import NotFound from "./components/NotFound.js";
 
 function App() {
   return (
     <Routes>
       <Route exact path='*' element={<NotFound />} />
+      <Route element={<AllPosts />} exact path='/allpost' />
+      <Route element={<OnePost />} path='/:slug' />
+      <Route element={<About />} exact path='/About' />
+      <Route element={<Portfolio />} exact path='/' />
+      <Route element={<Me />} exact path='/Gabriel-Abreu' />
+      <Route element={<Repos />} exact path='/Repositorios' />
     </Routes>
   );
 }
